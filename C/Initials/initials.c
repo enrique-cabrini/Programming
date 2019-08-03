ccf07ef42be6d613423955710fa6804658e497ef
enrique-cabrini <enrique.cabrini@gmail.com>
2019-08-03 09:51:57 +0000

    wrote a program that prints my initials in letters

---
+++ C/Initials/initials.c
@@ -0,0 +1,12 @@
+/* This program prints my initials in letters */
+#include <stdio.h>
+
+int main( void )
+{
+    /* Print initials */
+    printf( "EEEEEEEEE\nE   E   E\nE   E   E\nE   E   E\nE   E   E\n" );
+    printf( "\n  JJ     \n J       \nJ        \n J       \n  JJJJJJJ\n" );
+    printf( "\nDDDDDDDDD\nD       D\nD       D\n D     D \n  DDDDD  \n" );
+
+    return 0;
+} /* end function */
