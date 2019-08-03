a089daacd7d8f35ef65612ab39ce44faed187b97
enrique-cabrini <enrique.cabrini@gmail.com>
2019-08-03 09:11:57 +0000

    wrote a program that draws shapes

---
+++ C/AsteriskShapes/asteriskShapes.c
@@ -0,0 +1,18 @@
+/* This program draws some shapes with asterisks. */
+#include <stdio.h>
+
+int main( void )
+{
+    /* Draw shapes */
+    printf( "*********\t   ***   \t    *    \t    *    \n" );
+    printf( "*       *\t *     * \t   ***   \t   * *   \n" );
+    printf( "*       *\t*       *\t  *****  \t  *   *  \n" );
+    printf( "*       *\t*       *\t    *    \t *     * \n" );
+    printf( "*       *\t*       *\t    *    \t*       *\n" );
+    printf( "*       *\t*       *\t    *    \t *     * \n" );
+    printf( "*       *\t*       *\t    *    \t  *   *  \n" );
+    printf( "*       *\t *     * \t    *    \t   * *   \n" );
+    printf( "*********\t   ***   \t    *    \t    *    \n" );
+
+    return 0;
+} /* end function */
