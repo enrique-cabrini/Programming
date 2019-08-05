d56a52a090aa8c2d3f5d29c73c020134421631ae
enrique-cabrini <enrique.cabrini@gmail.com>
2019-08-05 13:47:09 +0000

    wrote a program that determines if a number is a multiple of another number

---
+++ C++/Multiples/multiples.cpp
@@ -0,0 +1,20 @@
+// This program determines if a number is a multiple of another number
+#include <iostream>
+
+int main()
+{
+    // Declare variables
+    int number1;
+    int number2;
+
+    std::cout << "Enter two numbers:\n"; // Prompt user to enter values
+    std::cin >> number1 >> number2; // Read values from user and store them in variables
+
+    if ( number1 % number2 == 0 )
+        std::cout << number1 << " is a multiple of " << number2 << std::endl;
+
+    if ( number1 % number2 != 0 )
+        std::cout << number1 << " is not a multiple of " << number2 << std::endl;
+
+    return 0;
+} // end function
