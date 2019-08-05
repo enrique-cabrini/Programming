2ca977c43012c043e653acb4613dfd639ba4d14a
enrique-cabrini <enrique.cabrini@gmail.com>
2019-08-05 10:39:47 +0000

    wrote a program to print the integer values of some characters

---
+++ C/IntegerValue/integerValue.c
@@ -0,0 +1,23 @@
+/* This program prints the integer value of some characters */
+#include <stdio.h>
+
+int main( void )
+{
+    /* Print the integer value of characters */
+    printf( "%d\n", 'A' );
+    printf( "%d\n", 'B' );
+    printf( "%d\n", 'C' );
+    printf( "%d\n", 'a' );
+    printf( "%d\n", 'b' );
+    printf( "%d\n", 'c' );
+    printf( "%d\n", '0' );
+    printf( "%d\n", '1' );
+    printf( "%d\n", '2' );
+    printf( "%d\n", '$' );
+    printf( "%d\n", '*' );
+    printf( "%d\n", '+' );
+    printf( "%d\n", '/' );
+    printf( "%d\n", ' ' );
+
+    return 0;
+} /* end function */
