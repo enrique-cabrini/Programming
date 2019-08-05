15103204b1549f47c1cd680e070def644b9688b4
enrique-cabrini <enrique.cabrini@gmail.com>
2019-08-05 12:49:21 +0000

    wrote a program that draws shapes with asterisks

---
+++ C++/Shapes/shapes.cpp
@@ -0,0 +1,18 @@
+// This program draws shapes with asterisks.
+#include <iostream>
+
+int main()
+{
+    // Print shapes
+    std::cout << "*********\t   ***   \t    *    \t    *    \n";
+    std::cout << "*       *\t *     * \t   ***   \t   * *   \n";
+    std::cout << "*       *\t*       *\t  *****  \t  *   *  \n";
+    std::cout << "*       *\t*       *\t    *    \t *     * \n";
+    std::cout << "*       *\t*       *\t    *    \t*       *\n";
+    std::cout << "*       *\t*       *\t    *    \t *     * \n";
+    std::cout << "*       *\t*       *\t    *    \t  *   *  \n";
+    std::cout << "*       *\t *     * \t    *    \t   * *   \n";
+    std::cout << "*********\t   ***   \t    *    \t    *    \n";
+
+    return 0;
+} // end function
