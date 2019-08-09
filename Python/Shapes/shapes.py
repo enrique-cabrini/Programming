6a225bac82029f6cd33b05077029757cbdf04836
enrique-cabrini <enrique.cabrini@gmail.com>
2019-08-09 10:50:21 +0000

    wrote a program that prints some shapes

---
+++ Python/Shapes/shapes.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# This program prints some shapes
+
+print( "*********\t   ***   \t    *    \t    *    " )
+print( "*       *\t *     * \t   ***   \t   * *   " )
+print( "*       *\t*       *\t  *****  \t  *   *  " )
+print( "*       *\t*       *\t    *    \t *     * " )
+print( "*       *\t*       *\t    *    \t*       *" )
+print( "*       *\t*       *\t    *    \t *     * " )
+print( "*       *\t*       *\t    *    \t  *   *  " )
+print( "*       *\t *     * \t    *    \t   * *   " )
+print( "*********\t   ***   \t    *    \t    *    " )
