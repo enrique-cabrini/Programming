624f0804d7764adc16dbecb08fb231bb23b4f2f4
enrique-cabrini <enrique.cabrini@gmail.com>
2019-08-11 17:03:50 +0000

    wrote a program that finds the factorial of a number

---
+++ Python/Factorial/factorial.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+# This program finds the factorial of a user specified number
+
+# Define variables
+number = 0;
+factorial = 0;
+counter = 0;
+# remainder = 0;
+temp_number = "";
+
+number = int( input( "Enter a nonnegative integer: " ) ) # Read number from user
+
+temp_number = str( number ) # Assign the value of number to temp_number
+
+factorial = number # Assign the value of number to factorial
+
+counter = number - 1 # Assign number minus 1 to counter
+
+# If number is greater than 0
+if number > 0:
+    # Use a loop to calculate the factorial
+    while counter >= 1:
+        factorial *= number - counter
+        counter -= 1; # Increment counter
+elif number == 0: # If number if 0
+    # Factorial is 1
+    factorial = 1
+else: # Otherwise
+    print( "Invalid input." ) # Print an error message
+
+print( "The factorial of {} is {}".format( number, factorial ) ) # Print the factorial
