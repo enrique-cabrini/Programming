86abf8a63b2c5083b5b37fa45e2cf2ded846537b
enrique-cabrini <enrique.cabrini@gmail.com>
2019-08-27 20:21:32 +0000

    fixed a little bug

--- C/HollowSquare/square.c
+++ C/HollowSquare/square.c
@@ -14,59 +14,65 @@ int main( void )
     printf( "Enter a number: " ); /* Prompt user for input */
     scanf( "%d", &number ); /* Read number from user */
 
-    /* Use a while loop to print row */
-    while ( row == 1 )
+    /* If number is greater than 1 */
+    if ( number > 1 )
     {
-        column = 1; /* Set column to 1 */
+        /* Use a while loop to print row */
+        while ( row == 1 )
+        {
+            column = 1; /* Set column to 1 */
+
+            /* Use a nested while to print each column in a row */
+            while ( column <= number )
+            {
+                printf( "*" ); /* Print asterisks */
+                column++; /* Increment column */
+            } /* end while */
+
+            printf( "\n" ); /* Go to a newline */
+            row++; /* Increment row */
+        } /* end while */
+
+        row = 1; /* Set row to 1 */
 
         /* Use a nested while to print each column in a row */
-        while ( column <= number )
+        while ( row <= number - 2 )
         {
-            printf( "*" ); /* Print asterisks */
-            column++; /* Increment column */
-        } /* end while */
+            column = 1; /* Set column to 1 */
 
-        printf( "\n" ); /* Go to a newline */
-        row++; /* Increment row */
-    } /* end while */
+            printf( "*" ); /* Print one asterisk at the beginning of each row */
 
-    row = 1; /* Set row to 1 */
+            /* Use a loop to print draw each column on a row */
+            while ( column <= number - 2 )
+            {
+                printf( " " ); /* Print a space */
+                column++; /* Increment column */
+            } /* end while */
 
-    /* Use a nested while to print each column in a row */
-    while ( row <= number - 2 )
-    {
-        column = 1; /* Set column to 1 */
+            printf( "*\n" ); /* Print an asterisk then go to the next line */
+            row++; /* Increment row */
+        }
 
-        printf( "*" ); /* Print one asterisk at the beginning of each row */
+        row = 1; /* Set row to 1 */
 
-        /* Use a loop to print draw each column on a row */
-        while ( column <= number - 2 )
+        /* Use a while loop to print row */
+        while ( row == 1 )
         {
-            printf( " " ); /* Print a space */
-            column++; /* Increment column */
+            column = 1; /* Set column to 1 */
+
+            /* Use a nested while to print each column in a row */
+            while ( column <= number )
+            {
+                printf( "*" ); /* Print asterisks */
+                column++; /* Increment column */
+            } /* end while */
+
+            printf( "\n" ); /* Go to a newline */
+            row++; /* Increment row */
         } /* end while */
-
-        printf( "*\n" ); /* Print an asterisk then go to the next line */
-        row++; /* Increment row */
-    }
-
-    row = 1; /* Set row to 1 */
-
-    /* Use a while loop to print row */
-    while ( row == 1 )
-    {
-        column = 1; /* Set column to 1 */
-
-        /* Use a nested while to print each column in a row */
-        while ( column <= number )
-        {
-            printf( "*" ); /* Print asterisks */
-            column++; /* Increment column */
-        } /* end while */
-
-        printf( "\n" ); /* Go to a newline */
-        row++; /* Increment row */
-    } /* end while */
+    } /* end if */
+    else /* Otherwise */
+        printf( "*\n" ); /* Print a single asterisk */
 
     return 0; /* Indicate the program ended successfully */
 } /* end function */
